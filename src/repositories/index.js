4af0a9b4e6f39645fd20ad43bbf73861198889f9
Minsung Kim <alstjddl0513@gmail.com>
2026-03-17 17:46:30 +0900

    Fix: 레포지토리 파일

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -2,7 +2,7 @@ export * from './user.repository.js';
 export * from './challenge.repository.js';
 export * from './submission.repository.js';
 export * from './heart.repository.js';
-export * from './UserWorkSpace.repository.js';
+export * from './userWorkspace.repository.js';
 export * from './editRequest.repository.js';
 export * from './feedback.repository.js';
 export * from './notification.repository.js';
