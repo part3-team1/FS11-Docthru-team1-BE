56710121dc32e1ae100af9f02f4ad5646bd4101b
odc0202 <apple@appleui-MacBookAir.local>
2026-03-18 18:39:55 +0900

    exception 코드

---
+++ src/common/exceptions/conflict.exception.js
@@ -0,0 +1,8 @@
+import { HttpException } from './http.exception.js';
+import { ERROR_MESSAGE } from '../constants/error.js';
+
+export class ConflictException extends HttpException {
+  constructor(message = ERROR_MESSAGE.CONFLICT, details = null) {
+    super(409, message, details);
+  }
+}
